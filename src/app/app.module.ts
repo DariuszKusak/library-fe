c0e6413b1d6d5fab6f709466c8fc190d5c6c24e4
DariuszKusak <dariusz.kusak@gmail.com>
2020-02-08 09:56:15 +0100

    Routing created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+import { InformationComponent } from './information/information.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+  {path : '', component : InformationComponent},
+  {path : 'books', component : BookListComponent},
+  {path : 'posts', component : PostListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    PostListComponent,
+    InformationComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
